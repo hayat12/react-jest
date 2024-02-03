d947c371a33d055fb6738e1bb3dfba2adc010d56
hayat <hayat@freightmark.com.my>
2024-02-03 17:43:38 +0100

    group test implemented

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,18 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './greet';
 
-/**
+
+/** Group Test */
+
+describe("Greet", ()=>{
+
+    /**
  * Greet should render the text hello and if the name is passed into the component
  * it should render hello followed by the name
  */
-test('Greet render correctly', () => {
+test('Render correctly', () => {
     render(<Greet/>);
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
 })
 
-test('Greet render with a name', () => { 
-    render(<Greet name={'hayat'}/>);
-    const textElement = screen.getByText(/Hello hayat/i);
-    expect(textElement).toBeInTheDocument();
+describe("Nested", ()=>{
+    test('Render with a name', () => { 
+        render(<Greet name={'hayat'}/>);
+        const textElement = screen.getByText(/Hello hayat/i);
+        expect(textElement).toBeInTheDocument();
+    })
+})
+
 })
