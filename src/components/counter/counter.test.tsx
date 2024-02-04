0712978bf1edbe406c21ee9126f3926da3081cd0
hayat <hayat@freightmark.com.my>
2024-02-04 15:16:44 +0100

    implemented some keyboard interaction

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -27,4 +27,53 @@ describe("Counter", ()=>{
         const textElement = screen.getByRole('heading');
         expect(textElement).toHaveTextContent('1');
     });
+
+    test('should render count of 10 after clicking the set', async () => {
+        user.setup();
+        render(<Counter />);
+        const amountInput = screen.getByRole('spinbutton');
+        await act(async () => {
+            await user.type(amountInput, "10");
+        })
+        expect(amountInput).toHaveValue(10);
+
+        const buttonElement = screen.getByRole('button', { name: /set/i });
+
+        await act(async () => {
+            await user.click(buttonElement);
+        });
+
+        const headingText = screen.getByRole('heading')
+        expect(headingText).toHaveTextContent("10")
+    })
+
+
+    test('should focus the element in right order', async () => { 
+        user.setup()
+        render(<Counter />);
+        const amountInput = screen.getByRole('spinbutton');
+        
+        const setButton = screen.getByRole('button', {
+            name: /set/i
+        });
+
+        const incrementButton = screen.getByRole('button', {
+            name: /increment/i
+          });
+
+          await act(async () => {
+                await user.tab();
+            });
+            expect(amountInput).toHaveFocus();
+        
+        await act( async()=>{
+            await user.tab();
+        })
+        expect(setButton).toHaveFocus();
+
+        await act( async()=>{
+            await user.tab();
+        })
+        expect(incrementButton).toHaveFocus();
+    })
 })
--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
   return (
     <div>
-    <label>Counter</label>
-    <h1>{count}</h1> <br />
-    <button onClick={()=>setCount(count+1)}>Increment</button>
+      <label>Counter</label>
+      <h1>{count}</h1> <br />
+      <input
+        type="number"
+        onChange={(event) => setAmount(parseInt(event.target.value))}
+        value={amount}
+        name="count"
+      />
+      <button onClick={() => setCount(amount)}>Set</button>
+      <br />
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
